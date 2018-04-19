51752cbd5544a6960b3272cc6d2d26abea877327
Thomas Rudin <thomas@rudin.io>
2018-04-19 13:07:09 +0000

    static tile gen stub

---
+++ src/main/java/io/rudin/minetest/tileserver/StaticTileGen.java
@@ -0,0 +1,10 @@
+package io.rudin.minetest.tileserver;
+
+public class StaticTileGen {
+
+	public static void main(String[] args) {
+		// TODO create tiles in directory with static html index file
+
+	}
+
+}
