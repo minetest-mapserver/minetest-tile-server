05a2ee1ab424d5edf0e04aa8dfa4595e6c8b5f7c
Thomas Rudin <thomas@rudin.io>
2018-06-03 09:25:23 +0200

    limit update result

--- src/main/java/io/rudin/minetest/tileserver/job/UpdateChangedTilesJob.java
+++ src/main/java/io/rudin/minetest/tileserver/job/UpdateChangedTilesJob.java
@@ -13,6 +13,7 @@ import org.jooq.Record2;
 import io.rudin.minetest.tileserver.service.TileCache;
 import io.rudin.minetest.tileserver.util.CoordinateResolver;
 import io.rudin.minetest.tileserver.util.CoordinateResolver.TileInfo;
+import org.jooq.Result;
 
 @Singleton
 public class UpdateChangedTilesJob implements Runnable {
@@ -44,49 +45,45 @@ public class UpdateChangedTilesJob implements Runnable {
 			running = true;
 
 
-			Cursor<Record2<Integer, Integer>> cursor = ctx
+			Result<Record2<Integer, Integer>> result = ctx
 					.select(TILESERVER_BLOCK_CHANGES.POSX, TILESERVER_BLOCK_CHANGES.POSZ)
 					.from(TILESERVER_BLOCK_CHANGES)
 					.where(TILESERVER_BLOCK_CHANGES.CHANGED.eq(true))
 					.groupBy(TILESERVER_BLOCK_CHANGES.POSZ, TILESERVER_BLOCK_CHANGES.POSX)
-					.fetchLazy();
-			try {
-				
-				for (Record2<Integer, Integer> change: cursor) {
-					Integer x = change.get(TILESERVER_BLOCK_CHANGES.POSX);
-					Integer z = change.get(TILESERVER_BLOCK_CHANGES.POSZ);
-					
+					.limit(100)
+					.fetch();
 
-					TileInfo tileInfo = CoordinateResolver.fromCoordinates(x, z);
-					
-					//remove all tiles in every zoom
-					for (int i=CoordinateResolver.MAX_ZOOM; i>=CoordinateResolver.MIN_ZOOM; i--) {
-						TileInfo zoomedTile = tileInfo.toZoom(i);
+			for (Record2<Integer, Integer> change: result) {
+				Integer x = change.get(TILESERVER_BLOCK_CHANGES.POSX);
+				Integer z = change.get(TILESERVER_BLOCK_CHANGES.POSZ);
 
-						EventBus.TileChangedEvent event = new EventBus.TileChangedEvent();
-						event.x = zoomedTile.x;
-						event.y = zoomedTile.y;
-						event.zoom = zoomedTile.zoom;
-						event.mapblockX = x;
-						event.mapblockZ = z;
-						eventBus.post(event);
 
-						tileCache.remove(zoomedTile.x, zoomedTile.y, zoomedTile.zoom);
-					}
+				TileInfo tileInfo = CoordinateResolver.fromCoordinates(x, z);
 
-					//TODO: atomic change
-					ctx
-					.update(TILESERVER_BLOCK_CHANGES)
-					.set(TILESERVER_BLOCK_CHANGES.CHANGED, false)
-					.where(TILESERVER_BLOCK_CHANGES.POSX.eq(x))
-					.and(TILESERVER_BLOCK_CHANGES.POSZ.eq(z))
-					.execute();
+				//remove all tiles in every zoom
+				for (int i=CoordinateResolver.MAX_ZOOM; i>=CoordinateResolver.MIN_ZOOM; i--) {
+					TileInfo zoomedTile = tileInfo.toZoom(i);
+
+					EventBus.TileChangedEvent event = new EventBus.TileChangedEvent();
+					event.x = zoomedTile.x;
+					event.y = zoomedTile.y;
+					event.zoom = zoomedTile.zoom;
+					event.mapblockX = x;
+					event.mapblockZ = z;
+					eventBus.post(event);
+
+					tileCache.remove(zoomedTile.x, zoomedTile.y, zoomedTile.zoom);
 				}
-				
-			} finally {
-				cursor.close();
 
+				//TODO: atomic change
+				ctx
+				.update(TILESERVER_BLOCK_CHANGES)
+				.set(TILESERVER_BLOCK_CHANGES.CHANGED, false)
+				.where(TILESERVER_BLOCK_CHANGES.POSX.eq(x))
+				.and(TILESERVER_BLOCK_CHANGES.POSZ.eq(z))
+				.execute();
 			}
+
 
 		} finally {
 			running = false;
