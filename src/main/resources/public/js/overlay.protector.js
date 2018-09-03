4daffbbf05a0488522e79eb476942a8fa641e47e
Thomas Rudin <thomas@rudin.io>
2018-09-03 18:08:55 +0200

    protectors work

--- src/main/resources/public/js/overlay.protector.js
+++ src/main/resources/public/js/overlay.protector.js
@@ -8,25 +8,45 @@
 
 	function updateProtectors(map){
 		var latlng = map.getCenter();
+		var zoom = map.getZoom();
+		layerGroup.clearLayers();
+
+        if (zoom < 12)
+            return; //too much info
+
 		m.request("protector/" + layerId + "/" + parseInt(latlng.lng) + "/" + parseInt(latlng.lat))
 		.then(function(list){
-			layerGroup.clearLayers();
 
-			geoJsonLayer = L.geoJSON();
+			geoJsonLayer = L.geoJSON([], {
+			    onEachFeature: function(feature, layer){
+			        if (feature.properties && feature.properties.popupContent) {
+                        layer.bindPopup(feature.properties.popupContent);
+                    }
+			    }
+			});
+
 			list.forEach(function(protector){
 				var feature = {
 					"type":"Feature",
 					"geometry": {
-						"type":"Point",
-						"coordinates":[protector.x,protector.z]
+						"type":"Polygon",
+						"coordinates":[[
+						    [protector.x-5,protector.z-5],
+						    [protector.x-5,protector.z+6],
+						    [protector.x+6,protector.z+6],
+						    [protector.x+6,protector.z-5],
+						    [protector.x-5,protector.z-5]
+						]]
 					},
-					"properties":{"name":protector.owner}
+					"properties":{
+					    "name": protector.owner,
+					    "popupContent": protector.owner
+					}
 				}
 				geoJsonLayer.addData(feature);
 			});
 
 			geoJsonLayer.addTo(layerGroup);
-			console.log(list);
 		});
 	}
 
