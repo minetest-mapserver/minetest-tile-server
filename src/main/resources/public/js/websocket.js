96c8e9c504b22597d73a878ac14cda0553edbc7f
Thomas Rudin <thomas@rudin.io>
2018-09-07 14:06:38 +0200

    reconnect websocket after error

--- src/main/resources/public/js/websocket.js
+++ src/main/resources/public/js/websocket.js
@@ -1,13 +1,24 @@
 
 (function(tileserver){
 
-    var wsUrl = location.protocol.replace("http", "ws") + "//" + location.host + location.pathname.substring(0, location.pathname.lastIndexOf("/")) + "/ws";
-    var ws = new WebSocket(wsUrl);
-    ws.onmessage = function(e){
-        var event = JSON.parse(e.data);
-        tileserver.websocketCallbacks.forEach(function(cb){
-            cb(event);
-        });
-    }
+	var wsUrl = location.protocol.replace("http", "ws") + "//" + location.host + location.pathname.substring(0, location.pathname.lastIndexOf("/")) + "/ws";
+
+	function connect(){
+		var ws = new WebSocket(wsUrl);
+		ws.onmessage = function(e){
+			var event = JSON.parse(e.data);
+			tileserver.websocketCallbacks.forEach(function(cb){
+				cb(event);
+			});
+		}
+
+		ws.onerror = function(e){
+			setTimeout(connect, 2500);
+		}
+
+	}
+
+	//Initial connect
+	connect();
 
 })(window.tileserver);
