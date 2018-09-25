bfdc3f947b05f8c5118993f156ae64d9f449257c
Thomas Rudin <thomas@rudin.io>
2018-09-25 16:10:34 +0200

    digiline message block

--- tileserver_mod/tileserver/init.lua
+++ tileserver_mod/tileserver/init.lua
@@ -3,8 +3,14 @@ tileserver = {
 }
 
 local MP = minetest.get_modpath("tileserver")
+local has_digiline_mod = minetest.get_modpath("digiline")
 
 dofile(MP.."/poi.lua")
 dofile(MP.."/train.lua")
+
+if has_digiline_mod then
+	dofile(MP.."/digimessage.lua")
+end
+
 
 print("[OK] Tileserver")
